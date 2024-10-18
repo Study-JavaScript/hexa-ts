2e06dc8d52df4483a0179e0a50488620d83a00d1
Study-JavaScript <sektersytout@gmail.com>
2024-10-18 15:12:02 +0200

    refactor(github): Delete application folder

--- application/repositories/likepost.d.ts
+++
@@ -1,5 +0,0 @@
-import { LikePost } from "domain/entities/likepost"
-
-export type LikePostRepository = {
-    create: (postId: number, userId: number) => Promise<LikePost>
-}
--- application/repositories/post.d.ts
+++
@@ -1,7 +0,0 @@
-export type PostRepository = {
-    create(postData: {title:string, content:string|null}, userId: number): Promise<Post>;
-    readAll(): Promise<Post[]>;
-    delete(id: number): Promise<Post>;
-    update(id: number, postData: Partial<Post>): Promise<Post>;
-
-}
